e80a4ec66dc9bc92ddca8e31fc8b9cd02d5e2527
svmi3195 <target1983@gmail.com>
2017-08-20 23:20:00 +0300

    basic nav with some css styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import Navbar from "./components/common/NavComponent.js";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <Navbar />
+        
       </div>
     );
   }
