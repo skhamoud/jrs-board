64f83511c8a924ac556a0fa34f905de5e5241026
Hamoud Khatar <skhamoud@gmail.com>
2017-08-23 14:22:30 +0000

    Add Bulma and Sass

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,6 +1,6 @@
 // The HomePage Container Component
 import React from "react";
 
-const HomePage = () => <h2>HomePage</h2>;
+const HomePage = () => <h2 className="title">HomePage</h2>;
 
 export default HomePage;
